e5cab55e3cfb528a8bf664a37e06dccbb3a69703
Kaj Siebert <kaj@k-si.com>
2022-06-04 09:26:39 +0200

    Fix help text

--- register/management/commands/send-reminders.py
+++ register/management/commands/send-reminders.py
@@ -4,7 +4,7 @@ from register.tasks import send_reminders
 
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Send reminders to signed-in users'
 
     def add_arguments(self, parser):
         parser.add_argument('--send', "-s", action="store_true")
